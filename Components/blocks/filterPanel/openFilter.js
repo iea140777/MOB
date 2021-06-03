0fbdf23fbadde0b24ee1eeac344e7b8e7ccc22e1
iea140777 <51355806+iea140777@users.noreply.github.com>
2021-06-03 13:33:31 +0300

    Update openFilter.js

--- Components/blocks/filterPanel/openFilter.js
+++ Components/blocks/filterPanel/openFilter.js
@@ -3,14 +3,39 @@ document.addEventListener("DOMContentLoaded", function () {
   //------------------ OPEN & CLOSE FILTER PANEL-------------------------
   const FilterContainer = document.querySelector('.FilterContainer');
   const Filter = document.querySelector('.Filter');
-  const openFilter = document.querySelector('.headerPagesFilterButton');
-  const closeFilter = Filter.querySelector('.FilterClose');
+  const openFilterButton = document.querySelector('.headerPagesFilterButton');
+  const closeFilterButton = Filter.querySelector('.FilterClose');
  
-  openFilter.addEventListener('click', function () {
+  openFilterButton.addEventListener('click', function () {
+    openFilter();
+  });
+ 
+  closeFilterButton.addEventListener('click', function () {
+    closeFilter();
+  });
+
+  document.addEventListener('click', function (e){
+    const target = e.target;
+    const filter = target == FilterContainer || FilterContainer.contains(target);
+    const filterActive = FilterContainer.classList.contains('FilterContainerOpen');
+    const openButton = target == openFilterButton || openFilterButton.contains(target);
+    if (!filter && !openButton && filterActive) {
+      closeFilter();
+    }
+  })
+
+  function openFilter () {
     FilterContainer.classList.remove('mainHidden');
-  });
- 
-  closeFilter.addEventListener('click', function () {
-    FilterContainer.classList.add('mainHidden');
-  });
+    setTimeout(function() {
+      FilterContainer.classList.add('FilterContainerOpen');
+    }, 50);
+  }
+
+  function closeFilter () {
+    FilterContainer.classList.remove('FilterContainerOpen');
+    setTimeout(function() {
+      FilterContainer.classList.add('mainHidden');
+    }, 260);
+  }
+  
 })
