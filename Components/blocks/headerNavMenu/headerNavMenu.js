d09b85f9feb46377254f46a0d303384124d11a6d
iea140777 <51355806+iea140777@users.noreply.github.com>
2021-06-03 11:51:07 +0300

    Update headerNavMenu.js

--- Components/blocks/headerNavMenu/headerNavMenu.js
+++ Components/blocks/headerNavMenu/headerNavMenu.js
@@ -5,9 +5,15 @@ const closeMenu = menuContainer.querySelector('.mainDropdownMenuClose');
 
 openMenu.addEventListener('click', function () {
   menuContainer.classList.remove('mainHidden');
+  setTimeout(function() {
+    menuContainer.classList.add('mainDropdownMenuContainerOpen');
+  }, 50);
 });
 
 closeMenu.addEventListener('click', function () {
-  menuContainer.classList.add('mainHidden');
+  menuContainer.classList.remove('mainDropdownMenuContainerOpen');
+  setTimeout(function() {
+    menuContainer.classList.add('mainHidden');
+    }, 250);
 });
 //------------------END OPEN & CLOSE DROPDOWN MENU------------------------
